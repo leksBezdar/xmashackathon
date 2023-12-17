56c9c682e33e52c5d4739fc775efdee75997368d
Alexey Volkov <vollkovalex1991@mail.ru>
2023-12-17 20:41:03 +0400

    Initial commit

---
+++ src/auth/models.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+from typing import Annotated
+
+from sqlalchemy import TIMESTAMP, Boolean, ForeignKey, JSON, String
+from sqlalchemy.orm import  Mapped, mapped_column
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import ARRAY
+
+
+from ..database import Base
+
+user_links = Annotated[list, mapped_column(ARRAY(JSON), nullable=False, default=[])]
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[str] = mapped_column(primary_key=True, index=True)
+    email: Mapped[str] = mapped_column(nullable=False, unique=True)
+    username: Mapped[str] = mapped_column(nullable=False, unique=True)
+    is_superuser: Mapped[bool] = mapped_column(nullable=False, default=False)
+    hashed_password: Mapped[str] = mapped_column(nullable=False)
+    metategs: Mapped[list] = mapped_column(ARRAY(String), nullable=False)
+    links: Mapped[user_links]
+    
+    
+
+
+class Refresh_token(Base):
+    __tablename__ = 'refresh_tokens'
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    refresh_token: Mapped[str] = mapped_column(index=True)
+    expires_at: Mapped[int]
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    user_id: Mapped[str] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
